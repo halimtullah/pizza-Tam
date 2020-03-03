29b794e325797b91a9e174ee0a8e3889c1122647
khadija <sumeyahassan34@gmail.com>
2020-03-03 11:32:55 +0300

    Add changes

--- js/scripts.js
+++ js/scripts.js
@@ -13,15 +13,12 @@ var pizzaTopping = ["Pepperoni","Mushroom","Extracheese","Onion","Tomato","Sausa
 
 $(document).ready(function() {
     $("form").submit(function(event) {
-        event.preventDefault();
-        var name=$("input#name").val();
-        var email=$("input#email").val();
-        var message=$("input#message").val();
-        if ($("input#name").val() && $("input#email").val()){
-            alert (name + ",Thank you. we have receive your message .")
-        }
-        // else {
-        //     alert("please fill your name and email!")
-        // }
-    })
-})
+    var name=$("input#name").val();
+    var email=$("input#email").val();
+    var message=$("input#message").val();
+    alert(name + "Thank you for contacting us.");
+    
+    event.preventDefault();
+    });
+ });
+
